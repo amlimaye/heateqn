1675a6aaaaa112e913456f40c9c42de70b8dd298
Aditya Limaye <amlimaye@berkeley.edu>
2017-12-31 18:07:13 -0800

    get working with virtualenv

--- scripts/analyze.py
+++ scripts/analyze.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python
-
 import numpy as np
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 from matplotlib import cm
 
@@ -8,11 +8,11 @@ def do_plot(x, dat, fname="fig.png"):
     nlines = dat.shape[1]
     cm_sub = np.linspace(0, 1, nlines)
     colors = [cm.coolwarm(c) for c in cm_sub]
-    
+
     plt.figure()
     for col_idx in range(nlines):
         plt.plot(x, [1.00] + dat[:,col_idx].tolist() + [0.15], 'o-', color=colors[col_idx])
-    
+
     plt.xlabel(r"$x'$")
     plt.ylabel(r"$T'$")
     plt.savefig(fname)
