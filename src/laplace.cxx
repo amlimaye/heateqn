88f57307ce25dcb255a53f653caeeeb188ea7059
Aditya Limaye <amlimaye@berkeley.edu>
2018-01-06 11:50:11 -0800

    improvements to LaplaceOperator1D *comment reformatting *add accessor methods *explicitly zero out elements of the boundary term

--- src/laplace.cxx
+++ src/laplace.cxx
@@ -15,9 +15,10 @@ LaplaceOperator1D::LaplaceOperator1D(const int a_npoints, const real_t a_right_b
 
     real_t scale = std::pow(m_dx, -2);
 
+    //construct the appropriate discretization of the laplacian matrix
     for (int k = 0; k < a_npoints; k++) {
-        m_laplacian_matrix(k, k) = -2*scale;            //-2 along the diagonal for all indices
-        if (k != a_npoints - 1) {                       //if not at the last entry, place 1 to the bottom and right
+        m_laplacian_matrix(k, k) = -2*scale;
+        if (k != a_npoints - 1) {                                  
             m_laplacian_matrix(k, k+1) = 1*scale;
             m_laplacian_matrix(k+1, k) = 1*scale;
         }
@@ -27,6 +28,9 @@ LaplaceOperator1D::LaplaceOperator1D(const int a_npoints, const real_t a_right_b
     m_boundary_term.resize(a_npoints);
     m_boundary_term(0) = scale;
     m_boundary_term(a_npoints - 1) = a_right_bc * scale;
+    for (int k = 1; k < a_npoints - 1; k++) {
+        m_boundary_term(k) = 0;
+    }
 }
 
 colvec_t LaplaceOperator1D::apply(const colvec_t& a_apply_to) {
--- src/laplace.hxx
+++ src/laplace.hxx
@@ -5,6 +5,11 @@ public:
     LaplaceOperator1D(const int a_npoints, const real_t a_right_bc);
     colvec_t apply(const colvec_t& a_apply_to);
 
+    //accessors
+    real_t              get_dx() const {return m_dx;};
+    const mat_t&        get_laplacian() const {return m_laplacian_matrix;};
+    const colvec_t&     get_boundary_term() const {return m_boundary_term;};
+
 private:
     real_t      m_dx;
     mat_t       m_laplacian_matrix;
