409af0cba406dedc1cbcd5b4a3fe461dae84376f
Aditya Limaye <amlimaye@berkeley.edu>
2018-01-09 01:33:40 -0800

    refactor BackwardEuler to use AffineTransforms

--- src/backward_euler.cxx
+++ src/backward_euler.cxx
@@ -1,20 +1,28 @@
 #include "backward_euler.hxx"
 
-BackwardEuler::BackwardEuler(colvec_t a_ic, real_t a_dt) :
-    m_curr_state(a_ic),
-    m_dt(a_dt)
-{};
+BackwardEuler::BackwardEuler(AffineTransform* a_transform, colvec_t a_ic, 
+                             real_t a_dt) {
+    m_curr_state = a_ic;
+    m_dt = a_dt;
 
-void BackwardEuler::take_timestep(const mat_t& a_operator, const colvec_t& a_boundary_term) {
-    mat_t identity;
-    identity.setIdentity(a_operator.rows(), a_operator.cols());
-
-    mat_t A = identity - m_dt * a_operator;
-
+    //cache an LLT decomposition of (I - dt * L) for doing matrix solves
     //LLT decomposition requires positive definite-ness, which works for the
     //laplacian, but this will need to be changed later for generalization
     //purposes
-    m_curr_state = A.llt().solve(m_curr_state + m_dt*a_boundary_term);
+    mat_t identity;
+    mat_t A;
+
+    auto scale_matrix = a_transform->get_scale();
+
+    identity.setIdentity(scale_matrix.rows(), scale_matrix.cols());
+    A = identity - m_dt * scale_matrix;
+    m_llt_decomposed = A.llt();
+
+    m_rhs_shift = m_dt * a_transform->get_shift();
+};
+
+void BackwardEuler::take_timestep() {
+    m_curr_state = m_llt_decomposed.solve(m_curr_state + m_rhs_shift);
 }
 
 const colvec_t& BackwardEuler::get_state() const {
--- src/backward_euler.hxx
+++ src/backward_euler.hxx
@@ -1,14 +1,17 @@
 #include "types.hxx"
+#include "affine_transform.hxx"
 
 class BackwardEuler {
 public:
-    BackwardEuler (colvec_t a_ic, real_t a_dt);
+    BackwardEuler (AffineTransform* a_transform, colvec_t a_ic, real_t a_dt);
 
-    void                take_timestep(const mat_t& a_operator, const colvec_t& a_boundary_term);
+    void                take_timestep();
     const colvec_t&     get_state() const;
     const real_t&       get_dt() const;
 
 protected:
-    colvec_t m_curr_state;
-    real_t m_dt;
+    colvec_t            m_curr_state;
+    real_t              m_dt;
+    Eigen::LLT<mat_t>   m_llt_decomposed;
+    colvec_t            m_rhs_shift;
 };
