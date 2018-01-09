a6672de04d475b6e8cb4a69c6f1b456df5364079
Aditya Limaye <amlimaye@berkeley.edu>
2018-01-09 01:14:48 -0800

    refactor LaplaceOperator1D as an AffineTransform

--- src/laplace.cxx
+++ src/laplace.cxx
@@ -1,57 +1,80 @@
 #include "laplace.hxx"
 #include <iostream>
 
-LaplaceOperator1D::LaplaceOperator1D(const int a_npoints, const real_t a_right_bc) {
+LaplaceOperator1D::LaplaceOperator1D(const int npoints, const real_t right_bc, 
+                                     const real_t scale_factor) {
     //compute the discretized difference
-    m_dx = 1.0 / (a_npoints + 1);
+    m_dx = 1.0 / (npoints + 1);
 
-    m_laplacian_matrix.resize(a_npoints, a_npoints);
+    m_laplacian_matrix.resize(npoints, npoints);
     //make the discretized Laplace operator
-    for (int i = 0; i < a_npoints; i++) {
-        for (int j = 0; j < a_npoints; j++) {
+    for (int i = 0; i < npoints; i++) {
+        for (int j = 0; j < npoints; j++) {
             m_laplacian_matrix(i, j) = 0;
         }
     }
 
     //compute the scale factor but don't multiply it into the laplacian yet
-    m_scale = std::pow(m_dx, -2);
+    m_scale_factor = scale_factor * std::pow(m_dx, -2);
 
     //construct the appropriate discretization of the laplacian matrix
-    for (int k = 0; k < a_npoints; k++) {
+    for (int k = 0; k < npoints; k++) {
         m_laplacian_matrix(k, k) = -2;
-        if (k != a_npoints - 1) {                                  
+        if (k != npoints - 1) {                                  
             m_laplacian_matrix(k, k+1) = 1;
             m_laplacian_matrix(k+1, k) = 1;
         }
     }
 
     //make the boundary corrector
-    m_boundary_term.resize(a_npoints);
+    m_boundary_term.resize(npoints);
     m_boundary_term(0) = 1.0;
-    m_boundary_term(a_npoints - 1) = a_right_bc;
-    for (int k = 1; k < a_npoints - 1; k++) {
+    m_boundary_term(npoints - 1) = right_bc;
+    for (int k = 1; k < npoints - 1; k++) {
         m_boundary_term(k) = 0;
     }
 }
 
-colvec_t LaplaceOperator1D::apply(const colvec_t& a_apply_to) {
-    //scale the result before returning to avoid precision loss
-    auto unscaled_result = m_laplacian_matrix * a_apply_to + m_boundary_term;
-    return unscaled_result * m_scale;
+colvec_t LaplaceOperator1D::apply(const colvec_t& x) const {
+    //scale the result at the end to avoid precision loss
+    auto unscaled_result = m_laplacian_matrix * x + m_boundary_term;
+    return unscaled_result * m_scale_factor;
+}
+
+colvec_t LaplaceOperator1D::scale(const colvec_t& x) const {
+    return m_laplacian_matrix * m_scale_factor * x;
+}
+
+colvec_t LaplaceOperator1D::shift(const colvec_t& x) const {
+    return x + m_scale_factor * m_boundary_term;
+}
+
+void LaplaceOperator1D::apply(colvec_t& x) const {
+    //scale the result at the end to avoid precision loss
+    auto unscaled_result = m_laplacian_matrix * x + m_boundary_term;
+    auto scaled_result = m_scale_factor * unscaled_result;
+    x = scaled_result;
+}
+
+void LaplaceOperator1D::scale(colvec_t& x) const {
+    x = m_scale_factor * m_laplacian_matrix * x;
+}
+
+void LaplaceOperator1D::shift(colvec_t& x) const {
+    x += m_boundary_term * m_scale_factor;
+}
+mat_t LaplaceOperator1D::get_scale() const {
+    return m_laplacian_matrix * m_scale_factor;
+}
+
+colvec_t LaplaceOperator1D::get_shift() const {
+    return m_boundary_term * m_scale_factor;
 }
 
 const mat_t& LaplaceOperator1D::get_laplacian() const {
     return m_laplacian_matrix;
 }
 
-const mat_t LaplaceOperator1D::get_scaled_laplacian() const {
-    return m_scale*m_laplacian_matrix;
-}
-
 const colvec_t& LaplaceOperator1D::get_boundary_term() const {
     return m_boundary_term;
-}
-
-const colvec_t LaplaceOperator1D::get_scaled_boundary_term() const {
-    return m_scale*m_boundary_term;
 }
--- src/laplace.hxx
+++ src/laplace.hxx
@@ -1,25 +1,31 @@
 #include "types.hxx"
+#include "affine_transform.hxx"
 
-class LaplaceOperator1D {
+class LaplaceOperator1D : public AffineTransform {
 public:
     //ctor
-    LaplaceOperator1D(const int a_npoints, const real_t a_right_bc);
+    LaplaceOperator1D(const int npoints, const real_t right_bc,
+                      const real_t scale_factor = 1.0);
 
-    //apply to an column vector
-    colvec_t apply(const colvec_t& a_apply_to);
+    colvec_t            apply(const colvec_t& x) const;
+    void                apply(colvec_t& x) const;
 
-    //accessors
+    colvec_t            scale(const colvec_t& x) const;
+    colvec_t            shift(const colvec_t& x) const;
+
+    void                scale(colvec_t& x) const;
+    void                shift(colvec_t& x) const;
+
+    mat_t               get_scale() const;
+    colvec_t            get_shift() const;
+
     real_t              get_dx() const {return m_dx;};
     const mat_t&        get_laplacian() const;
     const colvec_t&     get_boundary_term() const;
 
-    //Warning: these make copies!
-    const mat_t         get_scaled_laplacian() const;
-    const colvec_t      get_scaled_boundary_term() const;
-
 private:
     real_t      m_dx;
-    real_t      m_scale;
+    real_t      m_scale_factor;
     mat_t       m_laplacian_matrix;
     colvec_t    m_boundary_term;
 };
